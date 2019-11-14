0e409edcaac713e55bb0db6806aa1d8cb5e34ed6
Bhavik Mistry <43392058+iambhavikmistry@users.noreply.github.com>
2019-11-14 17:32:20 +0530

    Print Without Semicolon all method
    Print Without Semicolon all method

---
+++ PrintWithOutSemicolon.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#define PRINT printf("Your String")
+int main()
+{
+    if (printf("Your String \n") )
+    { }
+    {
+        while (!printf( "Your String \n" ))
+        { }
+    }
+    switch (printf("Your String \n" ))
+    {   }
+    if (PRINT)
+    {    }
+}
